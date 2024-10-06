43bbc39a505bb809e3abdac726bf8b52cec63fc4
Aloneboydsfyfskhgf <aasifansari18754@gmail.com>
2024-10-06 15:36:45 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -703,13 +703,13 @@ Also your tutorial will be Added Your Group..."""
     DEVELOPER_TXT = """
 special Thanks To ❤️ Developers -
 
--Dev 1 [Owner of this bot ]<a href='https://t.me/KingVj01'>VJ</a>
+-Dev 1 [Owner of this bot ]<a href='https://t.me/Mrkillersss'>ᴀʟᴏɴᴇ</a>
 
--Dev 2 <a href='https://t.me/vjbots_bot'>VJ</a>
+-Dev 2 <a href='https://t.me/xmovieshub56'>Public</a>
 
--Dev 3 <a href='https://t.me/vj_botz>VJ Botz</a>
+-Dev 3 <a href='https://t.me/lhgubhguygxdu</a>
 
-- Dev 4 <a href='https://t.me/vj_bots'>TEAM VJ</a>
+- Dev 4 <a href='https://t.me/lhhdsihhtuihsx'>Horror Sport</a>
 """
 
     RENAME_TXT = """
